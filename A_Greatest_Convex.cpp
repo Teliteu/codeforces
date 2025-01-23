017ba4c6f8c4c6eb6fb8acd78d6a5b5dcd946e1e
Mateus Adriano <mateus.opinheiro@gmail.com>
2025-01-23 17:26:16 -0300

    Create A_Greatest_Convex.cpp

---
+++ A_Greatest_Convex.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void resolu() {
+  long long p;
+  cin>>p;
+  if(p>1){
+    cout<<p-1<<endl;
+  }
+  else{
+    cout<<-1<<endl;
+  }
+}
+
+int main(){
+  long long t;
+  cin >> t;
+  while (t--){
+    resolu();
+  }
+}
